8f654f19c0664809f80bbd13184bec51798cb20f
yzjiangyang <yzjiangyang123@163.com>
2022-12-06 17:21:09 -0800

    login logout and login status unit tests

---
+++ testing/testcases.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.test import TestCase as DjangoTestCase
+
+
+class TestCase(DjangoTestCase):
+
+    def create_user(self, username, email=None, password=None):
+        if email is None:
+            email = '{}@gmail.com'.format(username)
+        if password is None:
+            password = 'correct password'
+
+        return User.objects.create_user(username, email, password)

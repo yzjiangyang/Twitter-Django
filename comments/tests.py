265255b98ef4b5abcd7d6ec89bd5d585e027fb96
yzjiangyang <yzjiangyang123@163.com>
2022-12-10 10:10:02 -0800

    implement comment model

---
+++ comments/tests.py
@@ -0,0 +1,18 @@
+from testing.testcases import TestCase
+
+
+class commentModelTests(TestCase):
+
+    def test_comment_model(self):
+        user = self.create_user('test_user')
+        tweet = self.create_tweet(user)
+        comment = self.create_comment(user, tweet)
+        self.assertEqual(
+            comment.__str__(),
+            '{} - {} says {} at tweet {}'.format(
+                comment.created_at,
+                user,
+                comment.content,
+                tweet
+            )
+        )

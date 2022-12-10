a2139a5aa4f101de002b41a1b7abbb870fc840ea
yzjiangyang <yzjiangyang123@163.com>
2022-12-10 11:21:07 -0800

    comment create api and unit tests

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -20,6 +20,10 @@ class UserSerializerForFriendship(UserSerializerForTweet):
     pass
 
 
+class UserSerializerForComment(UserSerializerForTweet):
+    pass
+
+
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()

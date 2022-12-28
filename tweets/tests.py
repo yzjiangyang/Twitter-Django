1edb62158ba8e42d9e3a476ec4ece696d9d742d4
yzjiangyang <yzjiangyang123@163.com>
2022-12-28 14:04:05 -0800

    tweet photo model

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,6 +1,7 @@
 from datetime import timedelta
 from testing.testcases import TestCase
-from tweets.models import Tweet
+from tweets.constants import TweetPhotoStatus
+from tweets.models import Tweet, TweetPhoto
 from utils.time_helpers import utc_now
 
 
@@ -12,3 +13,11 @@ class TweetTests(TestCase):
         tweet.created_at = utc_now() - timedelta(hours=10)
         tweet.save()
         self.assertEqual(tweet.hour_to_now, 10)
+
+    def test_upload_tweet_photo(self):
+        user = self.create_user('test_user')
+        tweet = self.create_tweet(user)
+        tweet_photo = TweetPhoto.objects.create(user=user, tweet=tweet)
+        self.assertEqual(tweet_photo.user, user)
+        self.assertEqual(TweetPhoto.objects.count(), 1)
+        self.assertEqual(tweet_photo.status, TweetPhotoStatus.PENDING)
